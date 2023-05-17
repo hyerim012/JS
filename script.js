c856c50d760aec7463abdd2218d1ed96a4e50c56
hyerim012 <phr01222@gmail.com>
2023-05-17 21:37:37 +0900

    session_hw

---
+++ script.js
@@ -0,0 +1,25 @@
+const todayDiv = document.getElementById("today");
+const timeDiv = document.getElementById("time");
+
+function getTime() {
+    let now = new Date();
+    let year = padZero(now.getFullYear());
+    let month = padZero(now.getMonth() + 1);
+    let date = padZero(now.getDate());
+    let hour = padZero(now.getHours());
+    let minute = padZero(now.getMinutes());
+    let second = padZero(now.getSeconds());
+
+    todayDiv.textContent = `${year}년 ${month}월 ${date}일`;
+    timeDiv.textContent = `${hour}시 ${minute}분 ${second}초`;
+
+}
+
+function padZero(data) {
+    return data < 10 ? `0${data}` : data;
+}
+
+getTime();
+setInterval(getTime, 1000);
+
+
